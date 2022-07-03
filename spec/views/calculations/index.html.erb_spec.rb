88b5d0c3098865050261138b112ee6ab46a2d4d5
Joseph Wambua <jjoshua@gitlab.com>
2022-07-03 12:40:18 +0200

    Fix view tests

--- spec/views/calculations/index.html.erb_spec.rb
+++ spec/views/calculations/index.html.erb_spec.rb
@@ -1,46 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "calculations/index", type: :view do
+  let!(:calc_1) { create(:calculation, basic_pay: 10_000) }
+  let!(:calc_2) { create(:calculation, basic_pay: 10_000) }
   before(:each) do
     assign(:calculations, [
-      Calculation.create!(
-        basic_pay: "9.99",
-        allowances: "9.99",
-        gross_pay: "9.99",
-        taxable_pay: "9.99",
-        tax: "9.99",
-        tax_relief: "9.99",
-        paye: "9.99",
-        nhif: "9.99",
-        nssf: "9.99",
-        net_pay: "9.99"
-      ),
-      Calculation.create!(
-        basic_pay: "9.99",
-        allowances: "9.99",
-        gross_pay: "9.99",
-        taxable_pay: "9.99",
-        tax: "9.99",
-        tax_relief: "9.99",
-        paye: "9.99",
-        nhif: "9.99",
-        nssf: "9.99",
-        net_pay: "9.99"
-      )
+      calc_1,
+      calc_2
     ])
   end
 
   it "renders a list of calculations" do
     render
-    assert_select "tr>td", text: "9.99".to_s, count: 2
-    assert_select "tr>td", text: "9.99".to_s, count: 2
-    assert_select "tr>td", text: "9.99".to_s, count: 2
-    assert_select "tr>td", text: "9.99".to_s, count: 2
-    assert_select "tr>td", text: "9.99".to_s, count: 2
-    assert_select "tr>td", text: "9.99".to_s, count: 2
-    assert_select "tr>td", text: "9.99".to_s, count: 2
-    assert_select "tr>td", text: "9.99".to_s, count: 2
-    assert_select "tr>td", text: "9.99".to_s, count: 2
-    assert_select "tr>td", text: "9.99".to_s, count: 2
+    assert_select 'tr>td', text: '10000.0'.to_s, count: 2
   end
 end
