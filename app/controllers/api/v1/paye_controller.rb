a9a2911f35c053c04e412787d494b482e90ea926
Joseph Wambua <jjoshua@gitlab.com>
2022-07-06 11:56:50 +0200

    Add paye calculation api

---
+++ app/controllers/api/v1/paye_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::PayeController < Api::BaseController
+  def index
+    @calculations = Calculation.all
+    render json: @calculations
+  end
+
+  def calculate
+    basic_pay = params[:basic_pay].to_f
+    if Calculation.where(basic_pay: basic_pay).exists?
+      calculation = Calculation.where(basic_pay: basic_pay).first
+    else
+      calculation = Calculation.create(basic_pay: basic_pay)
+    end
+    calculation.recalculate
+    render json: calculation
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   resources :calculations
   get 'welcome/index'
   root 'calculations#new'
+
+  # API routes
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :paye
+      get 'calculate_paye', to: 'paye#calculate'
+    end
+  end
 end
